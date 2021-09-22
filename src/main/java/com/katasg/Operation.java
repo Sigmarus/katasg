72334b2af1669f814f976745bbeac1b605bb7c44
jonathan.paugois <jonathan.paugois@wizway.fr>
2021-09-22 13:13:34 +0200

    Added operation implementation (tests passing)

--- src/main/java/com/katasg/Operation.java
+++ src/main/java/com/katasg/Operation.java
@@ -8,4 +8,24 @@ public class Operation {
     private BigDecimal amount;
     private Date date;
 
+    public Operation(OperationType operationType, BigDecimal amount) throws InvalidOperationAmountException {
+        if (amount == null || amount.signum() < 1) {
+            throw new InvalidOperationAmountException();
+        }
+        this.type = operationType;
+        this.amount = amount;
+        this.date = new Date();
+    }
+
+    public OperationType getType() {
+        return this.type;
+    }
+
+    public BigDecimal getAmount() {
+        return this.amount;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
 }
