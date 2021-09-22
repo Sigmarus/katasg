b1ed20cb21f19c446655dd23adc24eb2475e19ec
jonathan.paugois <jonathan.paugois@wizway.fr>
2021-09-22 13:45:11 +0200

    Add account logic

--- src/main/java/com/katasg/Account.java
+++ src/main/java/com/katasg/Account.java
@@ -1,10 +1,35 @@
 package com.katasg;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
     private BigDecimal balance;
     private List<Operation> operations;
     
+    public Account() {
+        this.balance = BigDecimal.ZERO;
+        this.operations = new ArrayList<>();
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    public List<Operation> getOperations() {
+        return this.operations;
+    }
+
+    public void deposit(final BigDecimal amount) throws InvalidOperationAmountException {
+        Operation operation = new Operation(OperationType.DEPOSIT, amount);
+        this.operations.add(operation);
+        this.balance = this.balance.add(amount);
+    }
+
+    public void withdraw(final BigDecimal amount) throws InvalidOperationAmountException {
+        Operation operation = new Operation(OperationType.WITHDRAWAL, amount);
+        this.operations.add(operation);
+        this.balance = this.balance.subtract(amount);
+    }
 }
