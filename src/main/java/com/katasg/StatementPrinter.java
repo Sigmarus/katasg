b6217a6fefac0b50b8c5dc30ff7c23772f47ce95
jonathan.paugois <jonathan.paugois@wizway.fr>
2021-09-22 14:02:59 +0200

    Added printer implementation

--- src/main/java/com/katasg/StatementPrinter.java
+++ src/main/java/com/katasg/StatementPrinter.java
@@ -1,5 +1,29 @@
 package com.katasg;
 
+import java.io.PrintStream;
+import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
+
 public class StatementPrinter {
     
+    public static void printOperations(final PrintStream out, final Account account) {
+        StringBuilder stringBuilder = new StringBuilder()
+            .append("DATE | OPERATION | AMOUNT")
+            .append(System.lineSeparator());
+        
+        account.getOperations().forEach(operation -> {
+            stringBuilder.append(new SimpleDateFormat("yyyy-MM-dd").format(operation.getDate())).append(" | ")
+            .append(operation.getType()).append(" | ")
+            .append(operation.getAmount().setScale(2, RoundingMode.HALF_EVEN).doubleValue())
+            .append(System.lineSeparator());
+        });
+
+        stringBuilder.append("-------------------------")
+            .append(System.lineSeparator())
+            .append("CURRENT BALANCE : ")
+            .append(account.getBalance().setScale(2, RoundingMode.HALF_EVEN).doubleValue())
+            .append(System.lineSeparator());
+
+        out.print(stringBuilder.toString());
+    }
 }
